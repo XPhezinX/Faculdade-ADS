762924a6066d75e66b06adfeb9b80fde55184858
XPhezinX <limaphelipe81@gmail.com>
2026-02-13 18:55:58 -0300

    adicionando exemplo de switch

--- 1-Trimestre/Desenvolvimento_Dinamico/Instrucoes_e_Repeticao.js
+++ 1-Trimestre/Desenvolvimento_Dinamico/Instrucoes_e_Repeticao.js
@@ -1,7 +1,34 @@
-//if e else
+//if e else --------------------------------------------------------
 
 let clima = 'chovendo';
 
 if (clima === 'chovendo') {
     console.log('Levar guarda-chuva, está chovendo');
 } else {console.log("Não precisa levar um guarda-chuva");}
+
+
+
+
+//switch é um interruptor --------------------------------------------------------
+
+function checkTrafficLight(lightColor) {
+
+    switch (lightColor) {
+        case 'verde':
+            console.log("Pode atravessar a rua");
+            break;
+
+        case 'amarelo':
+            console.log("Prepare-se para parar!");
+            break;
+
+        case 'vermelho':
+            console.log("Pare!");
+            break;
+        
+        default:
+            console.log("Cor inválida!");
+    }
+}
+
+checkTrafficLight('verde');
