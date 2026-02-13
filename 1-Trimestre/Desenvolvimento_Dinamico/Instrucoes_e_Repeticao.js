f2bcc6fbd408ba7f16b8b386909b4854b670f1b3
XPhezinX <limaphelipe81@gmail.com>
2026-02-13 17:57:51 -0300

    oragnizando as pastas de matérias

---
+++ 1-Trimestre/Desenvolvimento_Dinamico/Instrucoes_e_Repeticao.js
@@ -0,0 +1,7 @@
+//if e else
+
+let clima = 'chovendo';
+
+if (clima === 'chovendo') {
+    console.log('Levar guarda-chuva, está chovendo');
+} else {console.log("Não precisa levar um guarda-chuva");}
